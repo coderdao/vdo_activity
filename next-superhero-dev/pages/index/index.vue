775a4f18a32039b069a7347b0ccb85fc476bc169
abo_o <abo2013@foxmail.com>
2019-12-01 17:48:53 +0800

    update

--- next-superhero-dev/pages/index/index.vue
+++ next-superhero-dev/pages/index/index.vue
@@ -1,12 +1,12 @@
 <template>
-	<view>
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			这是首页
-			<text class="title">{{title}}</text>
-		</view>
-		<navigator url="../search/search">搜索页面</navigator>
-		<navigator url="../me/me">个人页面</navigator>
+	<view class="page">
+		<swiper :indicator-dots="true" :autoplay="true" :circular="true" class="swip_list">
+			<swiper-item v-for="(img,key) of img_list" :key="key">
+				<view class="swiper-item">
+					<image :src="img.img_src" class="swip_item_img"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -14,11 +14,48 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				'img_list': []
 			}
 		},
 		onLoad() {
-
+			let me = this;
+			
+			uni.request({
+			    url: 'https://m.aidalan.com/api/headimg', // 请求轮播数据
+					method:"GET",
+			    data: { text: 'uni.request' },
+					success:function( res ){
+						let json = res.data;
+						if ( json.ret !== 1 ) { 
+							uni.showModal({
+								content: 'app异常：数据请求失败，请返回',
+								showCancel: false
+							}); 
+							
+							return false;
+						}
+						
+						me.$data.img_list = json.content;
+					}
+/*	
+			    success: ( res ) => {
+						
+						let json = res.data;
+						if ( json.ret !== 1 ) { 
+							uni.showModal({
+								content: 'app异常：数据请求失败，请返回',
+								showCancel: false
+							}); 
+							
+							return false;
+						}
+						
+						this.$data.img_list = json.content;
+			    }
+*/
+			});
+			
 		},
 		methods: {
 
@@ -27,5 +64,5 @@
 </script>
 
 <style>
-	
+	@import url("index.css");
 </style>
